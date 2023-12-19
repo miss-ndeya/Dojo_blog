9f4137d31a0f74648db508a532b1fda6b6c3da5f
Awa_la_princesse <miss-ndeya>
2023-12-19 08:58:57 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Navbar from './Navbar';
+// import Home from './Home';
+import Outputting from './Outputting';
+
+function App() {
+
+  return (
+    <div className="App">
+        < Navbar />
+      <div className="content">
+        {/* < Home /> */}
+        < Outputting />
+       
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
