7a6c732a4f170095f00cdf121fb5c444a039229a
Awa_la_princesse <miss-ndeya>
2024-01-18 22:41:37 +0000

    useEffect Cleanup

--- src/useFetch.js
+++ src/useFetch.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState, useEffect } from 'react';
 
 const useFetch = (url) => {
   const [data, setData] = useState(null);
@@ -6,27 +6,35 @@ const useFetch = (url) => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    const abortCont = new AbortController();
+
     setTimeout(() => {
-      fetch(url)
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for that resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setIsPending(false);
-          setData(data);
-          setError(null);
-        })
-        .catch((err) => {
+      fetch(url, { signal: abortCont.signal })
+      .then(res => {
+        if (!res.ok) { 
+          throw Error('could not fetch the data for that resource');
+        } 
+        return res.json();
+      })
+      .then(data => {
+        setIsPending(false);
+        setData(data);
+        setError(null);
+      })
+      .catch(err => {
+        if (err.name === 'AbortError') {
+          console.log('fetch aborted')
+        } else {
           setIsPending(false);
           setError(err.message);
-        });
+        }
+      })
     }, 1000);
-  }, [url]);
 
-  return {data, isPending, error}
-};
+    return () => abortCont.abort();
+  }, [url])
 
+  return { data, isPending, error };
+}
+ 
 export default useFetch;
