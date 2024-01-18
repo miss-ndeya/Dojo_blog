d89999266e5a204d5092930c7ebcdcfc98aded7c
Awa_la_princesse <miss-ndeya>
2024-01-18 10:31:39 +0000

    ajout

--- src/Outputting.jsx
+++ src/Outputting.jsx
@@ -2,23 +2,28 @@ import { useState } from "react";
 import BlogList from "./BlogList";
 
 const Outputting = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
+  const [blogs, setBlogs] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    { title: "Welcome party!", body: "lorem ipsum...", author: "yoshi", id: 2 },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
 
-    const handleDelete = (id) => {
-        const newBlogs = blogs.filter(blog => blog.id !== id);
-        setBlogs(newBlogs);
-      }
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
 
-    return ( 
+  return (
     <div className="home">
-       < BlogList  blogs={blogs} title="All Blogs !" handleDelete={handleDelete}/>
-       {/* < BlogList  blogs={blogs.filter(blog => blog.author === "mario")} title="Mario's Blogs" /> */}
+      <BlogList blogs={blogs} title="All Blogs !" handleDelete={handleDelete} />
+      {/* < BlogList  blogs={blogs.filter(blog => blog.author === "mario")} title="Mario's Blogs" /> */}
     </div>
-     );
-}
- 
+  );
+};
+
 export default Outputting;
