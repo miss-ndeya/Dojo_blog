869414e9a50edc1df0402d6f6f7622ed562b9426
Awa_la_princesse <miss-ndeya>
2024-01-18 12:30:13 +0000

    Conditional Loading Message

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,24 +3,25 @@ import BlogList from "./BlogList";
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
-
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
-    fetch("http://localhost:8000/blogs")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => {
-        setBlogs(data);
-        console.log(data);
-      });
+    setTimeout(() => {
+      fetch("http://localhost:8000/blogs")
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          setIsPending(false);
+          setBlogs(data);
+        });
+    }, 1000);
   }, []);
 
   return (
     <div className="home">
-      {blogs && (
-        <BlogList blogs={blogs} title="All Blogs" />
-      )}
+      {isPending ? <div>Loading</div> : ""}
+      {blogs ? <BlogList blogs={blogs} title="All Blogs" /> : ""}
     </div>
   );
 };
