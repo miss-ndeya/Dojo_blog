48faf4a0cd8b8569906d4632ab1ca58f78265c0c
Awa_la_princesse <miss-ndeya>
2024-01-19 23:26:46 +0000

    Programmatic Redirects

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,11 @@ import BlogList from "./BlogList";
 import useFetch from "./useFetch";
 
 const Home = () => {
-  const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs")
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
@@ -10,10 +14,10 @@ const Home = () => {
         <div>{error}</div>
       ) : isPending ? (
         <div>Loading</div>
-      ) : blogs ? (
+      ) : blogs && blogs.length > 0 ? (
         <BlogList blogs={blogs} title="All Blogs" />
       ) : (
-        " "
+        <div>Aucun blogs pour le moment</div>
       )}
     </div>
   );
